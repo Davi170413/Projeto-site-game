d833d3b7b2a1c29a5355fc66f167ab2a32ba4641
Davi170413 <Davi.flcruz@gmail.com>
2025-06-09 20:17:07 -0300

    Add files via upload

---
+++ Site_Web/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Gamepedia</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/icon/icon.png">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+<!--Link do JS-->
+    <script defer src="./js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<!--Navbar do Site - Inicio-->
+ 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <img src="./img/icon/icon.png" alt="Gamepedia" height="50" aria-current="page" href="http://localhost/Web%20TCC/Site_Web/">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="http://localhost/Web%20TCC/Site_Web/#">Sobre o site</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Fotos</a></li>
+            <li><a class="dropdown-item" href="#">Gameplay</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Ajuda</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!--Fim do NavBar-->
+<br>
+<br>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <h3>Filme de Minecraft terá sequência, confirma diretor</h3>
+      <p>Jared Hess confirma que sequência do filme de Minecraft
+      está em desenvolvimento e terá Alex como nova protagonista</p>
+      <p>O diretor Jared Hess confirmou que o filme de Minecraft
+        ganhará uma sequência. Após a boa recepção da primeira
+        adaptação nos cinemas, Hess revelou ao site Deadline que
+        o novo longa já está em fase de planejamento. A continuação
+        trará a personagem Alex ao lado de Steve, interpretado
+        por Jack Black.</p>
+        <img src="./img/fotos/minecraft.jpeg" alt="minecraft" class="img-thumbnail">
+    </div>
+    <div class="col">
+      <h3>Super Mario Bros. - O Filme 2 chegará em 2026</h3>
+      <p>Super Mario Bros. - O Filme 2 já tem data de estreia
+        definida e a Nintendo divulgou a informação antes de
+        trazer seu Nintendo Direct</p>
+      <p>A Nintendo divulgou oficialmente que
+        Super Mario Bros. – O Filme 2 será lançado
+        no dia 24 de abril de 2026. A data está prevista para
+        o mercado japonês, podendo sofrer alterações de acordo
+        com cada região – isso sem contar o calendário de lançamentos,
+        que pode fazer a produção ser adiantada ou adiada de
+        acordo com o que for anunciado.</p>
+        <img src="./img/fotos/mario.jpeg" alt="mario" class="img-thumbnail">
+    </div>
+    <div class="col">
+      <h3>Filme Sonic 4 ganha data de estreia para 2027</h3>
+      <p>O sucesso comercial do terceiro filme fez a Paramount
+        Pictures correr para confirmar a data de Sonic 4</p>
+      <p>Pouco mais de um mês após confirmar que o filme
+        Sonic 4 já faz parte de seus planos, a Paramount
+        Pictures divulgou publicamente quando a animação
+        vai estrear. Em um comunicado enviado à Variety,
+        a companhia confirmou que ela chega aos cinemas dos
+        Estados Unidos no dia 19 de março de 2027.</p>
+        <img src="./img/fotos/sonic.jpeg" alt="sonic" class="img-thumbnail">
+    </div>
+  </div>
+  <!-- INICIO DA DIV DE TABELAS..--> 
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+          <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Top 5 - Jogos</th>
+            <th scope="col">Nome do Jogo</th>
+            <th scope="col">Lançamento</th>
+            <th scope="col">Desenvolvedora</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>God of War Ragnarok</td>
+            <td>9 de novembro de 2022</td>
+            <td>Santa Monica Studio</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Marvel's Spider-Man: Miles Morales</td>
+            <td>12 de novembro de 2020</td>
+            <td>Isomaniac Games</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Ratchet & Clank: Rift Apart</td>
+            <td>11 de junho de 2021</td>
+            <td>Isomaniac Games</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Horizon Forbidden West</td>
+            <td>18 de fevereiro de 2022</td>
+            <td>Guerrilla Games</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Final Fantasy XVI</td>
+            <td>22 de junho de 2023</td>
+            <td>Square Enix</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+</body>
+</html>
